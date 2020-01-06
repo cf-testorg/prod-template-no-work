bcb2fff00118762a3b4263488d6ca38fcfc20b3f
Cuma Cakmak <cuma.cakmak@devfactory.com>
2020-01-06 16:16:54 +0300

    Update RuleTestApp.java

--- src/main/java/com/restoratio/monaco/ruletest/RuleTestApp.java
+++ src/main/java/com/restoratio/monaco/ruletest/RuleTestApp.java
@@ -1,4 +1,4 @@
-package com.restoratio.monaco.ruletest;
+package com.restoratio.monaco.ruletest;  
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
